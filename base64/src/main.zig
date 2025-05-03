3ff2006c8f0f7fabf1f8596938c90ebfdc8cc6d5
Vaengir <vaengir@gmx.de>
2025-05-03 22:06:08 +0200

    feat: Basic mem space operations

--- base64/src/main.zig
+++ base64/src/main.zig
@@ -1,46 +1,87 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("base64_lib");
+const Base64 = struct {
+    _table: *const [64]u8,
+
+    fn init() Base64 {
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lower = "abcdefghijklmnopqrstuvwxyz";
+        const numbers_symb = "0123456789+/";
+        return Base64{
+            ._table = upper ++ lower ++ numbers_symb,
+        };
+    }
+
+    fn _char_at(self: Base64, index: usize) u8 {
+        return self._table[index];
+    }
+
+    fn _calc_encode_length(input: []const u8) !usize {
+        if (input.len < 3) {
+            return 4;
+        }
+        const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
+
+        return n_groups * 4;
+    }
+
+    fn _calc_decode_length(input: []const u8) !usize {
+        if (input.len < 3) {
+            return 3;
+        }
+        const n_groups: usize = try std.math.divFloor(usize, input.len, 4);
+        var multiple_groups: usize = n_groups * 3;
+        var i: usize = input.len - 1;
+        while (i > 0) : (i -= 1) {
+            if (input[i] == '=') {
+                multiple_groups -= 1;
+            } else {
+                break;
+            }
+        }
+
+        return multiple_groups;
+    }
+};
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
+    try stdout.print("Whatever\n", .{});
+    try stderr.print("Whatever\n", .{});
+}
+
+test "Index 28 return 'c'" {
+    const base64 = Base64.init();
+    const expected = 'c';
+    const result = base64._char_at(28);
+    try std.testing.expectEqual(expected, result);
+}
+
+test "Encode length min 4" {
+    try std.testing.expectEqual(4, Base64._calc_encode_length("Hi"));
+}
+
+test "Encode length 8" {
+    try std.testing.expectEqual(8, Base64._calc_encode_length("Halloi"));
+}
+
+test "Encode length 16" {
+    try std.testing.expectEqual(16, Base64._calc_encode_length("Halloelele"));
+}
+
+test "Decode length min 3" {
+    try std.testing.expectEqual(3, Base64._calc_decode_length("SG"));
+}
+
+test "Decode length 2" {
+    try std.testing.expectEqual(2, Base64._calc_decode_length("SGk="));
+}
+
+// test "Decode length 8" {
+//     try std.testing.expectEqual(8, Base64._calc_encode_length("Halloi"));
+// }
+
+// test "Decode length 16" {
+//     try std.testing.expectEqual(16, Base64._calc_encode_length("Halloelele"));
+// }
