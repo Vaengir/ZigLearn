4d14bba6f6f46759c05b152d0e107c7d2ad3c376
Vaengir <vaengir@gmx.de>
2025-05-04 10:47:05 +0200

    feat: Encode method

--- base64/src/main.zig
+++ base64/src/main.zig
@@ -12,6 +12,48 @@ const Base64 = struct {
         };
     }
 
+    fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+
+        const n_out = try _calc_encode_length(input);
+        var out = try allocator.alloc(u8, n_out);
+        var buf = [3]u8{ 0, 0, 0 };
+        var count: u8 = 0;
+        var iout: u64 = 0;
+
+        for (input, 0..) |_, i| {
+            buf[count] = input[i];
+            count += 1;
+            if (count == 3) {
+                out[iout] = self._char_at(buf[0] >> 2);
+                out[iout + 1] = self._char_at(((buf[0] & 0b00000011) << 4) + (buf[1] >> 4));
+                out[iout + 2] = self._char_at(((buf[1] & 0b00001111) << 2) + (buf[2] >> 6));
+                out[iout + 3] = self._char_at(buf[2] & 0b00111111);
+                iout += 4;
+                count = 0;
+            }
+
+            if (count == 1) {
+                out[iout] = self._char_at(buf[0] >> 2);
+                out[iout + 1] = self._char_at((buf[0] & 0b00000011) << 4);
+                out[iout + 2] = '=';
+                out[iout + 3] = '=';
+            }
+
+            if (count == 2) {
+                out[iout] = self._char_at(buf[0] >> 2);
+                out[iout + 1] = self._char_at(((buf[0] & 0b00000011) << 4) + (buf[1] >> 4));
+                out[iout + 2] = self._char_at((buf[1] & 0b00001111) << 2);
+                out[iout + 3] = '=';
+                iout += 4;
+            }
+        }
+
+        return out;
+    }
+
     fn _char_at(self: Base64, index: usize) u8 {
         return self._table[index];
     }
@@ -85,3 +127,13 @@ test "Decode length 2" {
 // test "Decode length 16" {
 //     try std.testing.expectEqual(16, Base64._calc_encode_length("Halloelele"));
 // }
+
+test "Encoded String Hi" {
+    var memory_buffer: [1000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
+    const allocator = fba.allocator();
+    const base64 = Base64.init();
+    const expected: []const u8 = "SGk=";
+    const result: []const u8 = try base64.encode(allocator, "Hi");
+    try std.testing.expect(std.mem.eql(u8, expected, result));
+}
